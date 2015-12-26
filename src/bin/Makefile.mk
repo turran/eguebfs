a86c5714c12131667066475f1d4dcb92c502ad67
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-12-26 20:50:45 +0100

    Improve the build system

--- src/bin/Makefile.mk
+++ src/bin/Makefile.mk
@@ -2,12 +2,12 @@ bin_PROGRAMS = \
 src/bin/eguebfs
 
 src_bin_eguebfs_LDADD = \
-$(top_builddir)/src/lib/libender.la \
-@ENDER_LIBS@
+$(top_builddir)/src/lib/libeguebfs.la \
+@EGUEBFS_LIBS@
 
 src_bin_eguebfs_CPPFLAGS = \
 -I$(top_srcdir)/src/lib \
-@ENDER_CFLAGS@
+@EGUEBFS_CFLAGS@
 
 src_bin_eguebfs_SOURCES = \
 src/bin/eguebfs.c
